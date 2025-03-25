6ed9e41678250642fe7e31ad79a3affb02c22fa2
Szymon <dembkowski123@gmail.com>
2025-03-25 22:04:50 +0100

    Fresh setup

---
+++ src/main/java/spidershop/SpidershopApplication.java
@@ -0,0 +1,13 @@
+package spidershop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpidershopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpidershopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/spidershop/SpidershopApplicationTests.java
@@ -0,0 +1,13 @@
+package spidershop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpidershopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
